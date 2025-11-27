76227e2a80ab4448812e6da839bd656afaa52b7e
Alan Shaw <alan@storacha.network>
2025-11-27 16:31:28 +0000

    feat: add json output

--- cmd/delegation/parse.go
+++ cmd/delegation/parse.go
@@ -5,6 +5,7 @@ import (
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/spf13/cobra"
+	"github.com/storacha/debugger/pkg/ipldfmt"
 	"github.com/storacha/debugger/pkg/ucanfmt"
 	"github.com/storacha/go-ucanto/core/delegation"
 )
@@ -12,9 +13,8 @@ import (
 var parseCmd = &cobra.Command{
 	Use:   "parse <value>",
 	Short: "Parse a delegation.",
-	Long: `Parse a multibase encoded CID, with an identity multihash that contains delegation data in a CAR file.
-`,
-	Args: cobra.MinimumNArgs(1),
+	Long:  `Parse a multibase encoded CID, with an identity multihash that contains delegation data in a CAR file.`,
+	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		logging.SetLogLevel("*", "info")
 
@@ -23,6 +23,27 @@ var parseCmd = &cobra.Command{
 			panic(fmt.Errorf("parsing delegation: %w", err))
 		}
 
-		ucanfmt.PrintDelegation(d)
+		jsonOutput, _ := cmd.Flags().GetBool("json")
+		if jsonOutput {
+			for b, err := range d.Blocks() {
+				if err != nil {
+					panic(fmt.Errorf("iterating delegation blocks: %w", err))
+				}
+				cmd.Printf("%s\n", b.Link())
+				s, err := ipldfmt.FormatDagCBOR(b.Bytes())
+				if err != nil {
+					panic(fmt.Errorf("formatting block %s: %w", b.Link(), err))
+				}
+				cmd.Println(s)
+				cmd.Println("")
+			}
+		} else {
+			ucanfmt.PrintDelegation(d)
+		}
 	},
+}
+
+func init() {
+	parseCmd.Flags().Bool("json", false, "Output DAG JSON")
+	Cmd.AddCommand(parseCmd)
 }
